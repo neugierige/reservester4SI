a1d5b8f4c402c802f5c735c98632992d8c8828ba
Luyuan Xing <lxing.nyc@gmail.com>
2016-07-07 20:43:55 -0400

    updated methods for Restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,22 +1,52 @@
 class RestaurantsController < ApplicationController
-  def index
-  end
+before_action :authenticate_owner!, except: [:show, :index]
 
-  def new
+
+  def index
+    @restaurants = Restaurant.all
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = current_owner.restaurants.new
   end
 
   def create
+    # @restaurant = Restaurant.new(restaurant_params)
+    @restaurant = current_owner.restaurants.new(restaurant_params)
+    if @restaurant.save
+      redirect_to @restaurant
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @restaurant = current_owner.restaurants.find(params[:id])
   end
 
   def update
+    @restaurant = current_owner.restaurants.find(params[:id])
+    if @restaurant.update(restaurant_params)
+      redirect_to @restaurant
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @restaurant = current_owner.restaurants.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurants_path
+  end
+
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :description, :address, :phone)
   end
 end
