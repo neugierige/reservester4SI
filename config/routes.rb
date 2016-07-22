8b898c488a8ccbf2d40cb16c5ab2b38e373df0b4
Luyuan Xing <lxing.nyc@gmail.com>
2016-07-22 15:41:39 -0400

    added users controller, raked routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root 'restaurants#index'
 
   devise_for :users
+  resources :user, only: [:index, :edit, :update, :destroy]
+  
   devise_for :owners
 
   resources :restaurants do
